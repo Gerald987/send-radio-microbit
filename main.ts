d3bd6cf3dc8adea6777ef60832794b7f17551add
Gerald987 <69228322+Gerald987@users.noreply.github.com>
2023-10-12 13:12:34 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendNumber(0)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendNumber(1)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendNumber(99)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendNumber(5)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+})
+radio.setGroup(66)
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    `)
